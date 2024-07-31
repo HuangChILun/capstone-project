a152e2895039b657f4f3d25d5011c7ea6acd7118
HuangChILun <60692423+HuangChILun@users.noreply.github.com>
2024-07-31 15:35:57 -0600

    fin

--- src/app/pages/Login/ForgotPassword/page.js
+++ src/app/pages/Login/ForgotPassword/page.js
@@ -7,10 +7,11 @@ export default function ForgotPassword() {
   const [email, setEmail] = useState('');
   const [verificationCode, setVerificationCode] = useState('');
   const [message, setMessage] = useState('');
-
+  
+   
   const handleSendCode = async () => {
     try {
-      const response = await fetch('http://localhost:8000/auth/forgot', {
+      const response = await fetch('https://capstone-project-backend-weld.vercel.app/auth/forgot', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
